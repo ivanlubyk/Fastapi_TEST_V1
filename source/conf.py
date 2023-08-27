31114b5f347cf20786266f25bca20e3141a1e912
Іван Лубик <126516121+ivanlubyk@users.noreply.github.com>
2023-08-27 23:52:10 +0300

    SECOND

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'fastapi_project_goit'
+copyright = '2023, Lubyk Ivan'
+author = 'Lubyk Ivan'
+release = 'Lubuk Ivan'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'Welcome to the Sphinx 7.2.3 quickstart utility.'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. fastapi_project_goit documentation master file, created by
+   sphinx-quickstart on Sun Aug 27 23:24:13 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to fastapi_project_goit's documentation!
+================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
